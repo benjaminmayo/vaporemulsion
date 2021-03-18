dbd22994cf49ddf732a9a67e25131330e3a89361
Benjamin Mayo <me@benjaminmayo.co.uk>
2021-03-18 20:06:30 +0000

    added product

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "vapor-emulsion",
     platforms: [.macOS(.v11)],
+    products: [
+        .library(name: "VaporEmulsion", targets: ["VaporEmulsion"]),
+    ],
     dependencies: [],
     targets: [
         .target(
