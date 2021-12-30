c8d8c51fe057262672a570aba9ac33a47205e164
Benjamin Mayo <me@bzamayo.com>
2021-12-30 00:12:47 +0000

    change version requirement

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "vapor-emulsion",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "VaporEmulsion", targets: ["VaporEmulsion"]),
     ],
